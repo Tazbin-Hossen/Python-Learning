5784dc85b4f54cf1096b79b699a2cbd5be66f3c6
Tazbin-Hossen <tazbin23@gmail.com>
2025-03-27 02:00:39 +0600

    variable

---
+++ variables.py
@@ -0,0 +1,11 @@
+# variable means container . It contains (number or text)
+
+school_name = "ABC primary School"
+monthly_fees = 1500
+print (school_name,monthly_fees)
+monthly_fees = 2000
+print (school_name,monthly_fees)
+vehical_fees = 149.5
+print (school_name)
+print (monthly_fees)
+print (vehical_fees)
