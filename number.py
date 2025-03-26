6aa86544b6abe5078a5d089d00adb0b5e382832d
Tazbin-Hossen <tazbin23@gmail.com>
2025-03-27 02:25:38 +0600

    number

---
+++ number.py
@@ -0,0 +1,18 @@
+a = 2 
+b = 4
+c = 3.5
+d = 10
+e = 6
+print (a+b+c+d)    # adding numbers
+print (d-b)        # subtraction between two numbers
+print (a*b)        # multipication between two numbers
+print (b/a)        #  b devided by a
+print (d%e)        # finding remainder when 'e' devide by 'd'
+print (2**3)       #  this means 2*2*2 = (2**3) 
+
+x = 5.6789
+print (round (x,2))
+
+z = b - 3.7
+print (z)
+print (round(z,1))
