ea15caec8b896b231843d7cfe1a72f723ecdf4fd
Tazbin-Hossen <tazbin23@gmail.com>
2025-03-29 12:54:20 +0600

    tuples

---
+++ tuples.py
@@ -0,0 +1,4 @@
+#tuples uses foe the storing group of values but the main difference of list and tuples are " list are (homogeneous and mutable) and tuples are (heterogeneous and immutable) "
+
+tup = (1,2)
+print(tup[0])
